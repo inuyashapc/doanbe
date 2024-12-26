21f1467d1741c35740291e304b997fc8558fe79f
inuyashapc162 <inuyashapc162@gmail.com>
2024-12-27 01:32:52 +0700

    3rd

--- src/modules/host/court/court.middleware.ts
+++ src/modules/host/court/court.middleware.ts
@@ -1,7 +1,7 @@
 import { AttributeKeyCourt } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import CustomError from '../../../outcomes/customError';
-import { AttributeCourtPayLoad } from './court.model';
+// import { AttributeCourtPayLoad } from './court.model';
 import attributeCourtHostService from './court.service';
 
 const attributeCourtHostMiddleware = {
@@ -11,17 +11,17 @@ const attributeCourtHostMiddleware = {
     next: NextFunction
   ) => {
     try {
-      const data: AttributeCourtPayLoad = req.body;
-      const attributeKeyCourtId = data.attributeKeyCourtId;
-      const attributeKeyCourt: AttributeKeyCourt | null =
-        await attributeCourtHostService.findAttributeKeyCourt(
-          attributeKeyCourtId
-        );
-      if (attributeKeyCourt) {
-        next();
-      } else {
-        next(new CustomError('Attribute Court does not exist.', 409));
-      }
+      // const data: AttributeCourtPayLoad = req.body;
+      // const attributeKeyCourtId = data.attributeKeyCourtId;
+      // const attributeKeyCourt: AttributeKeyCourt | null =
+      //   await attributeCourtHostService.findAttributeKeyCourt(
+      //     attributeKeyCourtId
+      //   );
+      // if (attributeKeyCourt) {
+      //   next();
+      // } else {
+      //   next(new CustomError('Attribute Court does not exist.', 409));
+      // }
     } catch (error: any) {
       next(new CustomError(error?.message, 500));
     }
